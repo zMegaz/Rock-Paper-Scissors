fb70b2787e3fd042a990951366f51705981fdc46
Megaz <megaxz12@yahoo.com>
2022-06-07 13:25:31 +0400

    e

--- javascript.js
+++ javascript.js
@@ -1,16 +1,35 @@
-/*function computerPlay() {
-  let myArray = ["Rock", "Paper", "Scissors"];
-  let rand = Math.floor(Math.random() * myArray.length);
-  if (rand === 1) {
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+
+function computerPlay() {
+  let computerPlay = ["Rock", "Paper", "Scissors"];
+  let computerSelection = Math.floor(Math.random() * computerPlay.length);
+  return computerSelection;
+  /* if (computerSelection === 1) {
     console.log("Rock");
-  } else if (rand === 2) {
+  } else if (computerSelection === 2) {
     console.log("Paper");
-  } else if (rand === 3) {
+  } else if (computerSelection === 3) {
     console.log("scissors");
+  }*/
+}
+/*function computerPlay() {
+  let computerPlay = ["Rock", "Paper", "Scissors"];
+  let computerSelection = Math.floor(Math.random() * computerPlay.length);
+  return computerPlay[computerSelection];
+  /*console.log(computerPlay[computerSelection]);
+}*/
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == "Rock" && computerSelection == 2) {
+    console.log("Player wins");
+  } else if (playerSelection == "Rock" && computerSelection == 3) {
+    console.log("Computer wins!");
+  } else if (playerSelection == "Rock" && computerSelection == 3) {
+    console.log("Computer wins!");
   }
 }
-computerPlay();*/
 
-let computerPlay = ["Rock", "Paper", "Scissors"];
-let rand = Math.floor(Math.random() * computerPlay.length);
-console.log(rand, computerPlay[rand]);
+function game() {}
+
+prompt();
