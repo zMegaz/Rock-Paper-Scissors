79b4010ff769ba539634c504ffe79526ddf99013
Megaz <megaxz12@yahoo.com>
2022-06-12 12:33:45 +0400

    Finished

--- javascript.js
+++ javascript.js
@@ -1,6 +1,3 @@
-const playerSelection = prompt("Please input Rock,Paper or Scissors");
-const computerSelection = computerPlay();
-
 function computerPlay() {
   let computerPlay = ["Rock", "Paper", "Scissors"];
   let computerSelection = Math.floor(Math.random() * computerPlay.length);
@@ -23,35 +20,34 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
   //rock
   if (playerSelection === "Rock" && computerSelection == 1) {
-    console.log("Tie");
+    return "Tie";
   } else if (playerSelection == "Rock" && computerSelection == 2) {
-    console.log("Computer wins!");
+    return "Computer wins!";
   } else if (playerSelection == "Rock" && computerSelection == 3) {
-    console.log("Player wins!");
+    return "Player wins!";
     //paper
   } else if (playerSelection == "Paper" && computerSelection == 1) {
-    console.log("Player wins!");
+    return "Player wins!";
   } else if (playerSelection == "Paper" && computerSelection == 2) {
-    console.log("Tie!");
+    return "Tie!";
   } else if (playerSelection == "Paper" && computerSelection == 3) {
-    console.log("Computer wins!");
+    return "Computer wins!";
   }
   //Scissors
   else if (playerSelection == "Scissors" && computerSelection == 1) {
-    console.log("Computer wins!");
+    return "Computer wins!";
   } else if (playerSelection == "Scissors" && computerSelection == 2) {
-    console.log("Player wins!");
+    return "Player wins!";
   } else if (playerSelection == "Scissors" && computerSelection == 3) {
-    console.log("Tie!");
+    return "Tie!";
   } else {
-    ("Please input Rock, Paper or Scissors");
+    return "Please input Rock, Paper or Scissors";
   }
 }
-playRound(playerSelection, computerSelection);
-/*function game() {
+function game() {
   //Play game 5 times
   for (let i = 0; i < 5; i++) {
-    const playerSelection = "Rock";
+    const playerSelection = prompt("Please input Rock,Paper or Scissors");
     const computerSelection = computerPlay();
     // Call playRound function, passing in newly returned values
     // from the playerPlay and computerPlay functions
@@ -59,4 +55,5 @@ playRound(playerSelection, computerSelection);
     // Log our result
     console.log(currentRound);
   }
-}*/
+}
+game();
